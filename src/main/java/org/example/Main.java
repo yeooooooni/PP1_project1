57d5463ecd246e983365bdc66a10a1db32accd36
yeon <22100141@handong.ac.kr>
2023-09-01 16:51:29 +0900

    Create Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        Student s1 = new Student("22100141", "김연희");
+        System.out.println(s1.toString());
+
+    }
+}
